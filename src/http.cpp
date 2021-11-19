135af3068f20ebe3aa89b21c046899426053e364
Aarnav Tale <aarnav@tale.me>
2021-11-19 23:41:35 +0000

    feat: add a 404 handler

--- src/http.cpp
+++ src/http.cpp
@@ -2,13 +2,25 @@
 
 uWS::App canister::http::http_server() {
 	auto server = uWS::App();
-	server.get("/healthz", [](uWS::HttpResponse<false> *res, uWS::HttpRequest *req) {
+	server.get("/healthz", [](uWS::HttpResponse<false> *res, __attribute__((unused)) uWS::HttpRequest *req) {
 		auto json = nlohmann::json({
 			{ "status", "OK" },
 			{ "timestamp", canister::util::timestamp() },
 		});
 
 		res->writeHeader("Content-Type", "application/json");
+		res->end(json.dump());
+	});
+
+	// uws tries globstar methods last so we can use it to catch 404s
+	server.any("/*", [](uWS::HttpResponse<false> *res, __attribute__((unused)) uWS::HttpRequest *req) {
+		auto json = nlohmann::json({
+			{ "status", "404 Not Found" },
+			{ "timestamp", canister::util::timestamp() },
+		});
+
+		res->writeHeader("Content-Type", "application/json");
+		res->writeStatus("404 Not Found");
 		res->end(json.dump());
 	});
 
